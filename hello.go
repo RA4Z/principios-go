867d2422b3687ecd7548be2ba642460fb443f1e3
RA4Z <rraz639@gmail.com>
2023-07-23 18:39:49 -0300

    Fazendo declarações de variáveis

--- hello.go
+++ hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	var nome string = "Robert"
+	var idade int = 19
+	var versao float32 = 1.1
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
 }
