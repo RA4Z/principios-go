6fc02f573801701905aa276d16f2056957afc945
RA4Z <rraz639@gmail.com>
2023-07-23 18:46:40 -0300

    Declarando variáveis de forma mais curta

--- hello.go
+++ hello.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var nome string = "Robert"
-	var idade int = 19
-	var versao float32 = 1.1
+	nome := "Robert"
+	idade := 19
+	versao := 1.1
 	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("--------------------------------")
+	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variável idade é", reflect.TypeOf(idade))
+	fmt.Println("O tipo da variável versão é", reflect.TypeOf(versao))
 }
