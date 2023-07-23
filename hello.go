4e69a8c906de3fff3d536ff367f74eca01b529d0
RA4Z <rraz639@gmail.com>
2023-07-23 18:56:35 -0300

    Escaneando a informação passada pelo usuário

--- hello.go
+++ hello.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	//"reflect"
 )
 
 func main() {
 	nome := "Robert"
-	idade := 19
 	versao := 1.1
-	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("--------------------------------")
-	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
-	fmt.Println("O tipo da variável idade é", reflect.TypeOf(idade))
-	fmt.Println("O tipo da variável versão é", reflect.TypeOf(versao))
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variável comando é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
+
+	// fmt.Println("--------------------------------")
+	// fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+	// fmt.Println("O tipo da variável idade é", reflect.TypeOf(idade))
+	// fmt.Println("O tipo da variável versão é", reflect.TypeOf(versao))
 }
