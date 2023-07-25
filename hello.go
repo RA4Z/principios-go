208da4a61c89ec44eb13c8f13367eba194b63d0a
RA4Z <rraz639@gmail.com>
2023-07-25 17:01:46 -0300

    Inserindo todo o código dentro de um loop

--- hello.go
+++ hello.go
@@ -10,21 +10,24 @@ import (
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
 
-	comando := lerComando()
+	for {
+		exibeMenu()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		comando := lerComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 
 	// if comando == 1 {
@@ -48,7 +51,7 @@ func main() {
 
 func exibeIntroducao() {
 	nome := "Robert"
-	versao := 1.1
+	versao := 1.2
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
@@ -70,5 +73,10 @@ func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
 	site := "https://www.alura.com.br"
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
+
 }
