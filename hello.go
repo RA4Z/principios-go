e57045616c18c282a8107952112b1cfa46ad5eb1
RA4Z <rraz639@gmail.com>
2023-07-25 10:39:45 -0300

    Controlando a aplicação com switch

--- hello.go
+++ hello.go
@@ -19,16 +19,27 @@ func main() {
 	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi", comando)
 
-	if comando == 1 {
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo Logs...")
+
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do Programa...")
+
+	// } else {
+	// 	fmt.Println("Não conheço este comando")
+	// }
+
+	switch comando {
+	case 1:
 		fmt.Println("Monitorando...")
-
-	} else if comando == 2 {
+	case 2:
 		fmt.Println("Exibindo Logs...")
-
-	} else if comando == 0 {
-		fmt.Println("Saindo do Programa...")
-
-	} else {
+	case 0:
+		fmt.Println("Saindo do programa")
+	default:
 		fmt.Println("Não conheço este comando")
 	}
 
