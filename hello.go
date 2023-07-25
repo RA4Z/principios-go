3ce43d22b559ec6b2556b1f593f7e0970a61010b
RA4Z <rraz639@gmail.com>
2023-07-25 17:12:02 -0300

    Adicionando array no projeto

--- hello.go
+++ hello.go
@@ -71,8 +71,17 @@ func lerComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
+	var sites [4]string
+	sites[0] = "https://www.alura.com.br"
+	sites[1] = "https://www.alura.com.br"
+	sites[2] = "https://www.caelum.com.br"
+	sites[3] = "https://www.alura.com.br"
+
+	fmt.Println(sites)
+
 	site := "https://www.alura.com.br"
 	resp, _ := http.Get(site)
+
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
